b36a2a96f8d2fb422828df59bcee663104cfe2c6
Maxiee <maxieewong@gmail.com>
2015-06-12 19:49:46 +0800

    fab add event implement.

--- app/src/main/java/com/maxiee/attitude/ui/AddEventActivity.java
+++ app/src/main/java/com/maxiee/attitude/ui/AddEventActivity.java
@@ -1,15 +1,24 @@
 package com.maxiee.attitude.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.maxiee.attitude.R;
+import com.maxiee.attitude.database.api.AddEventApi;
+
+import java.util.ArrayList;
 
 /**
  * Created by maxiee on 15-6-11.
  */
 public class AddEventActivity extends AppCompatActivity{
+    private EditText mEditEvent;
+    private EditText mEditFirstThought;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,36 @@ public class AddEventActivity extends AppCompatActivity{
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mEditEvent = (EditText) findViewById(R.id.edit_event);
+        mEditFirstThought = (EditText) findViewById(R.id.first_thought);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String event = mEditEvent.getText().toString();
+                String thought = mEditFirstThought.getText().toString();
+                ArrayList<String> labels = new ArrayList<String>();
+
+                if (event.isEmpty() || thought.isEmpty()) {
+                    Toast.makeText(AddEventActivity.this,
+                            getString(R.string.notempty),
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                labels.add("label1");
+                labels.add("label2");
+                labels.add("label3");
+                new AddEventApi(
+                        AddEventActivity.this,
+                        event,
+                        thought,
+                        labels).exec();
+            }
+        });
+
     }
 
 
