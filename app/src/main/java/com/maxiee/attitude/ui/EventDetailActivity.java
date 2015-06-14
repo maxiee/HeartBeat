f719f9557477eef1d6dd7d1de81209f01ff7ef1a
Maxiee <maxieewong@gmail.com>
2015-06-14 15:33:54 +0800

    EventDetailActivity.java: handle fab click to open dialog to add new thought.

--- app/src/main/java/com/maxiee/attitude/ui/EventDetailActivity.java
+++ app/src/main/java/com/maxiee/attitude/ui/EventDetailActivity.java
@@ -2,17 +2,20 @@ package com.maxiee.attitude.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.maxiee.attitude.R;
 import com.maxiee.attitude.database.api.GetOneEventApi;
 import com.maxiee.attitude.model.Event;
 import com.maxiee.attitude.ui.adapter.ThoughtTimeaxisAdapter;
+import com.maxiee.attitude.ui.dialog.NewThoughtDialog;
 
 import org.json.JSONException;
 
@@ -25,6 +28,8 @@ public class EventDetailActivity extends AppCompatActivity {
 
     private TextView mTvEvent;
     private RecyclerView mRecyclerView;
+    private ThoughtTimeaxisAdapter mAdapter;
+    private int mId;
 
     public static final String EXTRA_NAME = "id";
 
@@ -34,7 +39,7 @@ public class EventDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_detail);
 
         Intent intent = getIntent();
-        final int id = intent.getIntExtra(EXTRA_NAME, -1);
+        mId = intent.getIntExtra(EXTRA_NAME, -1);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,12 +49,34 @@ public class EventDetailActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
         try {
-            mEvent =  new GetOneEventApi(this, id).exec();
+            mEvent =  new GetOneEventApi(this, mId).exec();
             mTvEvent.setText(mEvent.getmEvent());
             mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-            mRecyclerView.setAdapter(new ThoughtTimeaxisAdapter(mEvent.getmThoughts()));
+            mAdapter = new ThoughtTimeaxisAdapter(mEvent.getmThoughts());
+            mRecyclerView.setAdapter(mAdapter);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewThoughtDialog dialog = new NewThoughtDialog(EventDetailActivity.this, mId);
+                dialog.setOnAddFinishedListener(new NewThoughtDialog.OnAddFinishedListener() {
+                    @Override
+                    public void update() {
+                        try {
+                            mEvent = new GetOneEventApi(EventDetailActivity.this, mId).exec();
+                            mAdapter = new ThoughtTimeaxisAdapter(mEvent.getmThoughts());
+                            mRecyclerView.setAdapter(mAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                dialog.show();
+            }
+        });
     }
 }
