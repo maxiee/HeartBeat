2ec14de31ebf3031053b5cb49b0062ddb9d6df3e
Maxiee <maxieewong@gmail.com>
2015-06-16 16:30:17 +0800

    TagView: modify from kaedea/Android-Cloud-TagView-Plus.

---
+++ app/src/main/java/com/maxiee/attitude/common/tagview/Constants.java
@@ -0,0 +1,37 @@
+package com.maxiee.attitude.common.tagview;
+
+import android.graphics.Color;
+
+/**
+ * Created by maxiee on 15-6-16.
+ *
+ * Modify from kaedea/Android-Cloud-TagView-Plus (https://github.com/kaedea/Android-Cloud-TagView-Plus)
+ *
+ */
+public class Constants {
+
+    //use dp and sp, not px
+
+    //----------------- separator TagView-----------------//
+    public static final float DEFAULT_LINE_MARGIN = 5;
+    public static final float DEFAULT_TAG_MARGIN = 5;
+    public static final float DEFAULT_TAG_TEXT_PADDING_LEFT = 8;
+    public static final float DEFAULT_TAG_TEXT_PADDING_TOP = 5;
+    public static final float DEFAULT_TAG_TEXT_PADDING_RIGHT = 8;
+    public static final float DEFAULT_TAG_TEXT_PADDING_BOTTOM = 5;
+
+    public static final float LAYOUT_WIDTH_OFFSET = 2;
+
+    //----------------- separator Tag Item-----------------//
+    public static final float DEFAULT_TAG_TEXT_SIZE = 14f;
+    public static final float DEFAULT_TAG_DELETE_INDICATOR_SIZE = 14f;
+    public static final float DEFAULT_TAG_LAYOUT_BORDER_SIZE = 0f;
+    public static final float DEFAULT_TAG_RADIUS = 10;
+    public static final int DEFAULT_TAG_LAYOUT_COLOR = Color.parseColor("#FF4081");
+    public static final int DEFAULT_TAG_LAYOUT_COLOR_PRESS = Color.parseColor("#88363636");
+    public static final int DEFAULT_TAG_TEXT_COLOR = Color.parseColor("#ffffff");
+    public static final int DEFAULT_TAG_DELETE_INDICATOR_COLOR = Color.parseColor("#ffffff");
+    public static final int DEFAULT_TAG_LAYOUT_BORDER_COLOR = Color.parseColor("#ffffff");
+    public static final String DEFAULT_TAG_DELETE_ICON = "×";
+    public static final boolean DEFAULT_TAG_IS_DELETABLE = false;
+}
