a1f9f60b39770f89533eb4c1d8881bbde1e11390
Maxiee <maxieewong@gmail.com>
2015-06-21 18:06:06 +0800

    Event.java: remove Thoughts filed (moving to a separate table).

--- app/src/main/java/com/maxiee/attitude/model/Event.java
+++ app/src/main/java/com/maxiee/attitude/model/Event.java
@@ -8,32 +8,20 @@ import org.json.JSONException;
  */
 public class Event {
     private String mEvent;
-    private Thoughts mThoughts;
     private long timestamp;
     private int mId;
 
     public Event(
             int id,
             String mEvent,
-            JSONArray thoughts,
             long timestamp) {
         this.mId = id;
         this.mEvent = mEvent;
-        this.mThoughts = new Thoughts(thoughts);
         this.timestamp = timestamp;
     }
 
-    public void addThought(String thought) throws JSONException {
-        mThoughts.addThought(thought);
-    }
-
-
     public String getmEvent() {
         return mEvent;
-    }
-
-    public Thoughts getmThoughts() {
-        return mThoughts;
     }
 
     public long getTimestamp() {
