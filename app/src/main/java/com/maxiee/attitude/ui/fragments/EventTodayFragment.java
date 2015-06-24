ad6c2632422e81177d6c0a781df46f01934916a9
Maxiee <maxieewong@gmail.com>
2015-06-24 17:27:28 +0800

    EventTodayFragment.java: show event today.

--- app/src/main/java/com/maxiee/attitude/ui/fragments/EventTodayFragment.java
+++ app/src/main/java/com/maxiee/attitude/ui/fragments/EventTodayFragment.java
@@ -2,21 +2,41 @@ package com.maxiee.attitude.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.maxiee.attitude.R;
+import com.maxiee.attitude.database.api.GetTodayEventApi;
+import com.maxiee.attitude.model.Event;
 import com.maxiee.attitude.ui.adapter.EventListAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by maxiee on 15-6-23.
  */
 public class EventTodayFragment extends Fragment{
 
+    private RecyclerView mRecyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_event_today, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+        updateEventList();
+
         return v;
+    }
+
+    public void updateEventList() {
+        ArrayList<Event>  eventList = new GetTodayEventApi(getActivity()).exec();
+        if (eventList != null) {
+            mRecyclerView.setAdapter(new EventListAdapter(eventList));
+        }
     }
 }
