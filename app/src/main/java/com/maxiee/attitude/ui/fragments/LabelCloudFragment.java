43d5ca655c2eeb56158e50918ccc8f42a548f107
Maxiee <maxieewong@gmail.com>
2015-06-26 18:24:17 +0800

    LabelCloudFragment.java: new Fragment showing cloud tag.

---
+++ app/src/main/java/com/maxiee/attitude/ui/fragments/LabelCloudFragment.java
@@ -0,0 +1,48 @@
+package com.maxiee.attitude.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.maxiee.attitude.R;
+import com.maxiee.attitude.common.cloudview.CloudView;
+import com.maxiee.attitude.database.api.GetLabelsAndFreqApi;
+import com.maxiee.attitude.database.api.GetOneLabelApi;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by maxiee on 15-6-24.
+ */
+public class LabelCloudFragment extends Fragment{
+
+    private CloudView mCloudView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_label_cloud, container, false);
+
+        Map<Integer,Integer> allLabels = new GetLabelsAndFreqApi(getActivity()).exec();
+        List<Pair<String, Integer>> labels = new LinkedList<>();
+
+        if (allLabels != null) {
+            ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>(allLabels.entrySet());
+            for (Map.Entry<Integer, Integer> label: list) {
+                labels.add(new Pair<String, Integer>(
+                        new GetOneLabelApi(getActivity(), label.getKey()).exec(),
+                        label.getValue()));
+            }
+        }
+
+        mCloudView = (CloudView) v.findViewById(R.id.cloud_view);
+        mCloudView.addLabels(labels);
+        return v;
+    }
+}
