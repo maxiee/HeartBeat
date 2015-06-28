51529f3788878a79eda1f32b20a826872821cbd8
Maxiee <maxieewong@gmail.com>
2015-06-28 18:06:38 +0800

    MainActivity.java: click settings menu opening SettingsActivity.

--- app/src/main/java/com/maxiee/attitude/ui/MainActivity.java
+++ app/src/main/java/com/maxiee/attitude/ui/MainActivity.java
@@ -1,13 +1,13 @@
 package com.maxiee.attitude.ui;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -125,6 +125,10 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent();
+            i.setAction(Intent.ACTION_MAIN);
+            i.setClass(MainActivity.this, SettingsActivity.class);
+            startActivity(i);
             return true;
         }
 
