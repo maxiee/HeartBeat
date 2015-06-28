14836d80820913f3eddf001348fa0ed262cfeff7
Maxiee <maxieewong@gmail.com>
2015-06-28 18:03:47 +0800

    EventImageRelationTable.java: a model to store relationship between events and iamges. not working now.

---
+++ app/src/main/java/com/maxiee/attitude/database/tables/EventImageRelationTable.java
@@ -0,0 +1,21 @@
+package com.maxiee.attitude.database.tables;
+
+/**
+ * Created by maxiee on 15-6-28.
+ */
+public class EventImageRelationTable {
+    public static final String NAME = "event_image_relation";
+
+    public static final String ID = "id";
+
+    public static final String EVENT_ID = "event_id";
+
+    public static final String IMAGE_ID = "image_id";
+
+    public static final String CREATE = "create table " + NAME
+            + "("
+            + ID + " integer primary key autoincrement,"
+            + EVENT_ID + " integer,"
+            + IMAGE_ID + " integer"
+            + ");";
+}
