3c8e9e49601c622b37b82b8079806e55015b0f95
Maxiee <maxieewong@gmail.com>
2015-07-01 22:08:52 +0800

    AddThoughtApi.java: new api.

--- app/src/main/java/com/maxiee/attitude/database/api/AddThoughtApi.java
+++ app/src/main/java/com/maxiee/attitude/database/api/AddThoughtApi.java
@@ -2,15 +2,8 @@ package com.maxiee.attitude.database.api;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 
-import com.maxiee.attitude.database.tables.EventsTable;
 import com.maxiee.attitude.database.tables.ThoughtsTable;
-import com.maxiee.attitude.model.Event;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Created by maxiee on 15-6-14.
@@ -36,7 +29,6 @@ public class AddThoughtApi extends BaseDBApi{
         values.put(ThoughtsTable.THOUGHT, mThought);
         values.put(ThoughtsTable.TIMESTAMP, System.currentTimeMillis());
 
-        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
         int thoughtKey = (int) add(ThoughtsTable.NAME, values);
 
         new AddEventThoughtRelationApi(
