36f7c93220a6e2a4c792f792274fa27dc6c9486d
Maxiee <maxieewong@gmail.com>
2015-07-02 15:09:35 +0800

    ScrollingFABBehavior.java: does not work.

--- app/src/main/java/com/maxiee/heartbeat/ui/common/ScrollingFABBehavior.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/common/ScrollingFABBehavior.java
@@ -1,11 +1,13 @@
 package com.maxiee.heartbeat.ui.common;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,11 +17,17 @@ import com.maxiee.heartbeat.R;
 
 /**
  * Created by maxiee on 15-7-2.
+ *
+ * https://guides.codepath.com/android/Floating-Action-Buttons
  */
 public class ScrollingFABBehavior extends FloatingActionButton.Behavior {
 
     private static final android.view.animation.Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
     private boolean mIsAnimatingOut = false;
+
+    public ScrollingFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
 
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
