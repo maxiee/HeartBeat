1706c7c9fa602df0f3e1a9d43234be90971fcf75
Maxiee <maxieewong@gmail.com>
2015-07-09 08:26:53 +0800

    DeleteThoughtByKeyApi.java: new api.

---
+++ app/src/main/java/com/maxiee/heartbeat/database/api/DeleteThoughtByKeyApi.java
@@ -0,0 +1,31 @@
+package com.maxiee.heartbeat.database.api;
+
+import android.content.Context;
+
+import com.maxiee.heartbeat.database.tables.EventThoughtRelationTable;
+import com.maxiee.heartbeat.database.tables.ThoughtsTable;
+
+/**
+ * Created by maxiee on 15-7-8.
+ */
+public class DeleteThoughtByKeyApi extends BaseDBApi{
+    private int mThoughtKey;
+
+    public DeleteThoughtByKeyApi(Context context, int thoughtKey) {
+        super(context);
+        mThoughtKey = thoughtKey;
+    }
+
+    public void exec() {
+        mDatabaseHelper.getWritableDatabase().delete(
+                EventThoughtRelationTable.NAME,
+                EventThoughtRelationTable.THOUGHT_ID + "=?",
+                new String[] {String.valueOf(mThoughtKey)}
+        );
+        mDatabaseHelper.getWritableDatabase().delete(
+                ThoughtsTable.NAME,
+                ThoughtsTable.ID + "=?",
+                new String[] {String.valueOf(mThoughtKey)}
+        );
+    }
+}
