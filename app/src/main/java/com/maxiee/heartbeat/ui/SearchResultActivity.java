1c998b532a3f7edff1ecf0649880cbdc093be45b
Maxiee <maxieewong@gmail.com>
2015-07-22 15:56:01 +0800

    SearchResultActivity.java: call EventSearchApi.java to search.

--- app/src/main/java/com/maxiee/heartbeat/ui/SearchResultActivity.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/SearchResultActivity.java
@@ -3,11 +3,18 @@ package com.maxiee.heartbeat.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
 import com.maxiee.heartbeat.R;
+import com.maxiee.heartbeat.database.api.EventSearchApi;
+import com.maxiee.heartbeat.model.Event;
+import com.maxiee.heartbeat.ui.adapter.EventListAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by maxiee on 15-7-21.
@@ -15,6 +22,7 @@ import com.maxiee.heartbeat.R;
 public class SearchResultActivity extends AppCompatActivity{
     private static final String TAG = SearchResultActivity.class.getSimpleName();
     private Toolbar mToolbar;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +33,15 @@ public class SearchResultActivity extends AppCompatActivity{
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
+
         Intent i = getIntent();
         String search = i.getStringExtra("search");
 
         if (!search.isEmpty()) {
             setTitle(search);
+            updateEventList(search);
         }
 
         Log.d(TAG, search);
@@ -43,5 +55,12 @@ public class SearchResultActivity extends AppCompatActivity{
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void updateEventList(String search) {
+        ArrayList<Event> eventList = new EventSearchApi(this, search).exec();
+        if (eventList != null) {
+            mRecyclerView.setAdapter(new EventListAdapter(eventList));
+        }
     }
 }
