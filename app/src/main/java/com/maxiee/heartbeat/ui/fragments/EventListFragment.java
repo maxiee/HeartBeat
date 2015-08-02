fca1599fd0f154e0115e9330266111b614547e90
Maxiee <maxieewong@gmail.com>
2015-08-02 17:13:03 +0800

    EventListFragment.java: show hint image when list is empty.

--- app/src/main/java/com/maxiee/heartbeat/ui/fragments/EventListFragment.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/fragments/EventListFragment.java
@@ -7,7 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
 import com.maxiee.heartbeat.R;
 import com.maxiee.heartbeat.database.api.GetAllEventApi;
 import com.maxiee.heartbeat.model.Event;
@@ -21,20 +24,36 @@ import java.util.ArrayList;
 public class EventListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private RelativeLayout mEmptyLayout;
+    private ImageView mImageEmpty;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mRecyclerView = (RecyclerView) inflater.inflate(
-                R.layout.fragment_event_list, container, false);
+        View v = (View) inflater.inflate(R.layout.fragment_event_list, container, false);
+        mEmptyLayout = (RelativeLayout) v.findViewById(R.id.empty);
+        mImageEmpty = (ImageView) v.findViewById(R.id.image_empty);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerview);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(mRecyclerView.getContext()));
         updateEventList();
-        return mRecyclerView;
+        return v;
     }
 
     public void updateEventList() {
         ArrayList<Event> eventList = new GetAllEventApi(getActivity()).exec();
-        if (eventList != null) {
+        if (!eventList.isEmpty()) {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mEmptyLayout.setVisibility(View.GONE);
             mRecyclerView.setAdapter(new EventListAdapter(eventList));
+        } else {
+            mRecyclerView.setVisibility(View.GONE);
+            mEmptyLayout.setVisibility(View.VISIBLE);
+            Glide.with(getActivity()).load(R.drawable.empty_bg).into(mImageEmpty);
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateEventList();
     }
 }
