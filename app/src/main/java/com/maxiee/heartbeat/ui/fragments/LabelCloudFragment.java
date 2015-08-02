904e73642408ee64039c4a2008f63cc45174cff9
Maxiee <maxieewong@gmail.com>
2015-08-02 17:36:55 +0800

    LabelCloudFragment.java: show hint when labels is empty.

--- app/src/main/java/com/maxiee/heartbeat/ui/fragments/LabelCloudFragment.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/fragments/LabelCloudFragment.java
@@ -3,11 +3,15 @@ package com.maxiee.heartbeat.ui.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.bumptech.glide.Glide;
 import com.maxiee.heartbeat.R;
 import com.maxiee.heartbeat.common.cloudview.CloudView;
 import com.maxiee.heartbeat.database.api.GetLabelsAndFreqApi;
@@ -25,6 +29,9 @@ import java.util.Map;
 public class LabelCloudFragment extends Fragment{
 
     private CloudView mCloudView;
+    private NestedScrollView mCloudLayout;
+    private RelativeLayout mEmptyLayout;
+    private ImageView mImageEmpty;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +39,9 @@ public class LabelCloudFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_label_cloud, container, false);
 
         mCloudView = (CloudView) v.findViewById(R.id.cloud_view);
+        mCloudLayout = (NestedScrollView) v.findViewById(R.id.cloud_layout);
+        mEmptyLayout = (RelativeLayout) v.findViewById(R.id.empty);
+        mImageEmpty = (ImageView) v.findViewById(R.id.image_empty);
 
         updateCloud();
 
@@ -52,17 +62,30 @@ public class LabelCloudFragment extends Fragment{
             }
         }
 
-        mCloudView.addLabels(labels);
-        mCloudView.setOnLabelClickListener(new CloudView.OnLabelClickListener() {
-            @Override
-            public void onClick(String label) {
-                Intent i = new Intent();
-                i.setAction(Intent.ACTION_MAIN);
-                i.setClass(getActivity(), LabelDetailActivity.class);
-                i.putExtra("tag_text", label);
-                startActivity(i);
-            }
-        });
+        if (!labels.isEmpty()) {
+            mCloudLayout.setVisibility(View.VISIBLE);
+            mEmptyLayout.setVisibility(View.GONE);
+            mCloudView.addLabels(labels);
+            mCloudView.setOnLabelClickListener(new CloudView.OnLabelClickListener() {
+                @Override
+                public void onClick(String label) {
+                    Intent i = new Intent();
+                    i.setAction(Intent.ACTION_MAIN);
+                    i.setClass(getActivity(), LabelDetailActivity.class);
+                    i.putExtra("tag_text", label);
+                    startActivity(i);
+                }
+            });
+        } else {
+            mCloudLayout.setVisibility(View.GONE);
+            mEmptyLayout.setVisibility(View.VISIBLE);
+            Glide.with(getActivity()).load(R.drawable.empty_bg1).into(mImageEmpty);
+        }
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateCloud();
     }
 }
