e5f87870746a71eb9914219dbea9bdcc8bec6d8a
Maxiee <maxieewong@gmail.com>
2015-09-15 14:19:14 +0800

    Thoughts.java: resource in Thought.

--- app/src/main/java/com/maxiee/heartbeat/model/Thoughts.java
+++ app/src/main/java/com/maxiee/heartbeat/model/Thoughts.java
@@ -1,7 +1,6 @@
 package com.maxiee.heartbeat.model;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 /**
@@ -53,10 +52,17 @@ public class Thoughts {
     }
 
     public static class Thought {
+        public static final String THOUGHT_RES = "thought_res";
+        public static final String THOUGHT_PATH = "thought_path";
+        public static final int HAS_NO_RES = -1;
+        public static final String HAS_NO_PATH = "";
+        public static final int RES_IMAGE = 0;
 
         private int key;
         private String mThought;
         private long mTimeStamp;
+        private int mResType = HAS_NO_RES;
+        private String mResPath;
 
         public Thought(int key, String mThought, long mTimeStamp) {
             this.key = key;
@@ -78,6 +84,12 @@ public class Thoughts {
 
         public void setThought(String mThought) {
             this.mThought = mThought;
+        }
+
+        public void setTypeAndPath(int resType, String path) {
+            if (resType == HAS_NO_RES) return;
+            mResType = resType;
+            mResPath = path;
         }
     }
 }
