65fb54e557da60167c5e79a71a554c97d6662d78
Maxiee <maxieewong@gmail.com>
2015-09-15 14:15:53 +0800

    AddThoughtApi.java: add thought resource.

--- app/src/main/java/com/maxiee/heartbeat/database/api/AddThoughtApi.java
+++ app/src/main/java/com/maxiee/heartbeat/database/api/AddThoughtApi.java
@@ -3,7 +3,9 @@ package com.maxiee.heartbeat.database.api;
 import android.content.ContentValues;
 import android.content.Context;
 
+import com.maxiee.heartbeat.database.tables.ThoughtResTable;
 import com.maxiee.heartbeat.database.tables.ThoughtsTable;
+import com.maxiee.heartbeat.model.Thoughts;
 
 /**
  * Created by maxiee on 15-6-14.
@@ -12,15 +14,21 @@ public class AddThoughtApi extends BaseDBApi{
 
     private int mEventKey;
     private String mThought;
+    private int mResType;
+    private String mResPath;
     private Context mContext;
 
     public AddThoughtApi(Context context,
                          final int eventKey,
-                         final String thought) {
+                         final String thought,
+                         final int resType,
+                         final String resPath) {
         super(context);
         mContext = context;
         mEventKey = eventKey;
         mThought = thought;
+        mResType = resType;
+        mResPath = resPath;
     }
 
     public boolean exec() {
@@ -35,6 +43,15 @@ public class AddThoughtApi extends BaseDBApi{
                 mContext,
                 mEventKey,
                 thoughtKey).exec();
+
+        if (mResType == Thoughts.Thought.HAS_NO_RES) return true;
+
+        ContentValues resValues = new ContentValues();
+        resValues.put(ThoughtResTable.THOUGHT_ID, thoughtKey);
+        resValues.put(ThoughtResTable.TYPE, mResType);
+        resValues.put(ThoughtResTable.PATH, mResPath);
+
+        int resKey = (int) add(ThoughtResTable.NAME, resValues);
 
         return true;
     }
