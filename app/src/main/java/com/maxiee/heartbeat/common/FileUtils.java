5869c7c7318f3bf7225285ef6fd86aac25b64b1d
maxiee <maxieewong@gmail.com>
2015-09-23 13:04:38 +0800

    FileUtils.java: saveLongImage.

--- app/src/main/java/com/maxiee/heartbeat/common/FileUtils.java
+++ app/src/main/java/com/maxiee/heartbeat/common/FileUtils.java
@@ -3,16 +3,56 @@ package com.maxiee.heartbeat.common;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by maxiee on 15-9-5.
  */
 public class FileUtils {
+    private final static String HB_PATH = "HeartBeat";
+    public static final String LONG_IMAGE_PATH = "LongImage";
+
+    public static String saveLongImage(Context context, Bitmap bitmap) {
+        try {
+
+            File heartBeatPath = new File(Environment.getExternalStorageDirectory(), HB_PATH);
+            if (!heartBeatPath.exists()) {
+                heartBeatPath.mkdir();
+            }
+            File longImagePath = new File(heartBeatPath, LONG_IMAGE_PATH);
+            if (!longImagePath.exists()) {
+                longImagePath.mkdir();
+            }
+            File longImageFile = new File(
+                    longImagePath,
+                    "[" + LONG_IMAGE_PATH + "][" + TimeUtils.getDate(context) + "].png"
+            );
+            longImageFile.createNewFile();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
+            byte[] bytes = os.toByteArray();
+
+            FileOutputStream fos = new FileOutputStream(longImageFile);
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+            return longImageFile.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
     @SuppressLint("NewApi")
     public static String uriToPath(Context context, Uri uri) {
         boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
