959523dad8508c70b521345760489a754392471e
maxiee <maxieewong@gmail.com>
2015-09-24 12:05:47 +0800

    EventDetailActivity.java: long image code refactoring.

--- app/src/main/java/com/maxiee/heartbeat/ui/EventDetailActivity.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/EventDetailActivity.java
@@ -3,13 +3,16 @@ package com.maxiee.heartbeat.ui;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -56,6 +59,8 @@ public class EventDetailActivity extends AppCompatActivity {
 
     private static final int ADD_IMAGE = 1127;
 
+    private static final int LONG_IMAGE_IMAGE_MAX_HEIGHT = 300;
+
     private Event mEvent;
 
     private TextView mTvEvent;
@@ -70,6 +75,7 @@ public class EventDetailActivity extends AppCompatActivity {
     private TextView mAddImageText;
     private String mImagePath;
     private Thoughts mThoughts;
+    private String mSortingType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +84,9 @@ public class EventDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         mId = intent.getIntExtra(EXTRA_NAME, -1);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        mSortingType = sp.getString("time_axis_sorting", "0");
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -247,71 +256,13 @@ public class EventDetailActivity extends AppCompatActivity {
     private void generateLongImage() {
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int height = 0;
         int width = displaymetrics.widthPixels;
 
-        if (mImagePath != null) {
-            height += mImageBackDrop.getMeasuredHeight();
-        }
-
-        height += mCardEvent.getMeasuredHeight();
-
-        int rvItemNum = mThoughts.length();
         FrameLayout fl = new FrameLayout(this);
+        fl.setBackgroundColor(ContextCompat.getColor(this, R.color.window_background));
         View item = getLayoutInflater().inflate(R.layout.item_thought_timeaxis, fl);
-        TextView tv = (TextView) item.findViewById(R.id.tv_thought);
-        ImageView iv = (ImageView) item.findViewById(R.id.image_thought);
-        CardView cv = (CardView) item.findViewById(R.id.card);
-        item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        int screenWidth = tv.getMeasuredWidth();
-        iv.setVisibility(View.GONE);
-        int imageMaxHeight = 300;
 
-        // 尺寸获取
-        for (int i=0; i<rvItemNum; i++) {
-            tv.setText(mThoughts.get(i).getThought());
-            if (mThoughts.get(i).hasImage()) {
-                iv.setVisibility(View.VISIBLE);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeFile(mThoughts.get(i).getPath(), options);
-                int imageWidth = options.outWidth;;
-                int imageHeight = options.outHeight;
-                int inSampleSize = 1;
-
-                if (imageWidth > screenWidth || width > screenWidth) {
-
-                    final int halfHeight = imageHeight / 2;
-                    final int halfWidth = imageWidth / 2;
-
-                    while ((halfHeight / inSampleSize) > imageMaxHeight
-                            && (halfWidth / inSampleSize) > imageMaxHeight) {
-                        inSampleSize *= 2;
-                    }
-                }
-                BitmapFactory.Options options2 = new BitmapFactory.Options();
-                options2.inSampleSize = inSampleSize;
-                Bitmap bmp =  BitmapFactory.decodeFile(mThoughts.get(i).getPath(), options2);
-                iv.setImageBitmap(bmp);
-
-//                iv.setVisibility(View.VISIBLE);
-//                BitmapFactory.Options options = new BitmapFactory.Options();
-//                options.inJustDecodeBounds = true;
-//                BitmapFactory.decodeFile(mThoughts.get(i).getPath(), options);
-//                int imageWidth = options.outWidth;
-//                int imageHeight = options.outHeight;
-//                float ratio = screenWidth * 1f / imageWidth;
-//                Log.d("maxiee", "带图感想比例值:" + String.valueOf(ratio));
-//                int screenHeight = (int) ((float) imageHeight * ratio);
-//                Log.d("maxiee", "带图感想高度测量:" + String.valueOf(screenHeight));
-//                height += screenHeight;
-//                imageHeightList[i] = screenHeight;
-            } else {
-                iv.setVisibility(View.GONE);
-            }
-            item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-            height += item.getMeasuredHeight();
-        }
+        int height = measureViews(item, width);
 
         Log.d("maxiee", "生成Bitmap:" + String.valueOf(width) + "," + String.valueOf(height));
         Bitmap bitmap = Bitmap.createBitmap(
@@ -320,68 +271,7 @@ public class EventDetailActivity extends AppCompatActivity {
                 Bitmap.Config.ARGB_8888);
         Canvas bitmapHolder = new Canvas(bitmap);
 
-        int yPos = 0;
-        if (mImagePath != null) {
-            int backDropHeight = mImageBackDrop.getMeasuredHeight();
-            mImageBackDrop.layout(0, 0, width, backDropHeight);
-            mImageBackDrop.buildDrawingCache();
-            Bitmap backDropBitmap = mImageBackDrop.getDrawingCache();
-            if (backDropBitmap != null) {
-                bitmapHolder.drawBitmap(backDropBitmap, 0, 0, null);
-            }
-            yPos += backDropHeight;
-        }
-
-        int cardEventHeight = mCardEvent.getMeasuredHeight();
-        mCardEvent.layout(0, 0, width, cardEventHeight);
-        mCardEvent.buildDrawingCache();
-        Bitmap eventBitmap = mCardEvent.getDrawingCache();
-        if (eventBitmap != null) {
-            bitmapHolder.drawBitmap(eventBitmap, 0, yPos, null);
-        }
-        yPos += cardEventHeight;
-
-        Bitmap itemBitmap;
-        int childHeight;
-        for (int i=0; i<rvItemNum; i++) {
-            tv.setText(mThoughts.get(i).getThought());
-            if (mThoughts.get(i).hasImage()) {
-                iv.setVisibility(View.VISIBLE);
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeFile(mThoughts.get(i).getPath(), options);
-                int imageWidth = options.outWidth;;
-                int imageHeight = options.outHeight;
-                int inSampleSize = 1;
-
-                if (imageWidth > screenWidth || width > screenWidth) {
-
-                    final int halfHeight = imageHeight / 2;
-                    final int halfWidth = imageWidth / 2;
-
-                    while ((halfHeight / inSampleSize) > imageMaxHeight
-                            && (halfWidth / inSampleSize) > imageMaxHeight) {
-                        inSampleSize *= 2;
-                    }
-                }
-                BitmapFactory.Options options2 = new BitmapFactory.Options();
-                options2.inSampleSize = inSampleSize;
-                Bitmap bmp =  BitmapFactory.decodeFile(mThoughts.get(i).getPath(), options2);
-                iv.setImageBitmap(bmp);
-            } else {
-                iv.setVisibility(View.GONE);
-            }
-            item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-            childHeight = item.getMeasuredHeight();
-            item.layout(0, 0, width, childHeight);
-            item.buildDrawingCache();
-            itemBitmap = item.getDrawingCache();
-            if (itemBitmap != null) {
-                bitmapHolder.drawBitmap(itemBitmap, 0, yPos, null);
-            }
-            item.destroyDrawingCache();
-            yPos += childHeight;
-        }
+        drawViews(item, bitmapHolder, width);
 
         final String savedPath = FileUtils.saveLongImage(EventDetailActivity.this, bitmap);
         if (!savedPath.isEmpty()) {
@@ -409,5 +299,144 @@ public class EventDetailActivity extends AppCompatActivity {
             });
             builder.show();
         }
+    }
+
+    private int measureViews(View item, int width) {
+        int height = 0;
+
+        if (mImagePath != null) {
+            height += mImageBackDrop.getMeasuredHeight();
+        }
+
+        height += mCardEvent.getMeasuredHeight();
+
+        TextView tv = (TextView) item.findViewById(R.id.tv_thought);
+        ImageView iv = (ImageView) item.findViewById(R.id.image_thought);
+        CardView cv = (CardView) item.findViewById(R.id.card);
+        item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        int screenWidth = tv.getMeasuredWidth();
+        iv.setVisibility(View.GONE);
+
+        // 尺寸获取
+        int rvItemNum = mThoughts.length();
+        for (int i=0; i<rvItemNum; i++) {
+            tv.setText(mThoughts.get(i).getThought());
+            if (mThoughts.get(i).hasImage()) {
+                iv.setVisibility(View.VISIBLE);
+                Bitmap bmp = loadBitmap(screenWidth, LONG_IMAGE_IMAGE_MAX_HEIGHT, mThoughts.get(i).getPath());
+                iv.setImageBitmap(bmp);
+            } else {
+                iv.setVisibility(View.GONE);
+            }
+            item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            height += item.getMeasuredHeight();
+        }
+        return height;
+    }
+
+    private void drawViews(View item, Canvas bitmapHolder, int width) {
+        TextView tv = (TextView) item.findViewById(R.id.tv_thought);
+        ImageView iv = (ImageView) item.findViewById(R.id.image_thought);
+        CardView cv = (CardView) item.findViewById(R.id.card);
+        TextView tvOrder = (TextView) item.findViewById(R.id.tv_order);
+        TextView tvTime = (TextView) item.findViewById(R.id.tv_time);
+        item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        int screenWidth = tv.getMeasuredWidth();
+        int yPos = 0;
+        // draw backdrop
+        if (mImagePath != null) {
+            int backDropHeight = mImageBackDrop.getMeasuredHeight();
+            mImageBackDrop.layout(0, 0, width, backDropHeight);
+            mImageBackDrop.buildDrawingCache();
+            Bitmap backDropBitmap = mImageBackDrop.getDrawingCache();
+            if (backDropBitmap != null) {
+                bitmapHolder.drawBitmap(backDropBitmap, 0, 0, null);
+            }
+            yPos += backDropHeight;
+        }
+        // draw cardview
+        int cardEventHeight = mCardEvent.getMeasuredHeight();
+        mCardEvent.layout(0, 0, width, cardEventHeight);
+        mCardEvent.buildDrawingCache();
+        Bitmap eventBitmap = mCardEvent.getDrawingCache();
+        if (eventBitmap != null) {
+            bitmapHolder.drawBitmap(eventBitmap, 0, yPos, null);
+        }
+        yPos += cardEventHeight;
+        // drww item
+        Bitmap itemBitmap;
+        int childHeight;
+        int rvItemNum = mThoughts.length();
+        for (int i=0; i<rvItemNum; i++) {
+            tv.setText(mThoughts.get(i).getThought());
+            tvOrder.setText(getOrder(i, mThoughts.length()));
+            long time = mThoughts.get(i).getTimeStamp();
+            tvTime.setText(TimeUtils.parseTime(this, time));
+            if (mThoughts.get(i).hasImage()) {
+                iv.setVisibility(View.VISIBLE);
+                Bitmap bmp = loadBitmap(screenWidth, LONG_IMAGE_IMAGE_MAX_HEIGHT, mThoughts.get(i).getPath());
+                iv.setImageBitmap(bmp);
+            } else {
+                iv.setVisibility(View.GONE);
+            }
+            item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            childHeight = item.getMeasuredHeight();
+            item.layout(0, 0, width, childHeight);
+            item.buildDrawingCache();
+            itemBitmap = item.getDrawingCache();
+            if (itemBitmap != null) {
+                bitmapHolder.drawBitmap(itemBitmap, 0, yPos, null);
+            }
+            item.destroyDrawingCache();
+            yPos += childHeight;
+        }
+    }
+
+    private Bitmap loadBitmap(int reqWidth, int reqHeight, String path) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+        int imageWidth = options.outWidth;;
+        int imageHeight = options.outHeight;
+        int inSampleSize = 1;
+
+        if (imageWidth > reqWidth) {
+
+            final int halfHeight = imageHeight / 2;
+            final int halfWidth = imageWidth / 2;
+
+            while ((halfHeight / inSampleSize) > reqHeight) {
+                inSampleSize *= 2;
+            }
+        }
+        options.inJustDecodeBounds = false;
+        options.inSampleSize = inSampleSize;
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    private String getOrder(int position, int length) {
+        String order;
+        if (mSortingType.equals("0")) {
+            switch (position) {
+                case 0:
+                    order = getString(R.string.firtime);
+                    break;
+                case 1:
+                    order = getString(R.string.sectime);
+                    break;
+                default:
+                    order = String.valueOf(position + 1) + ".";
+                    break;
+            }
+        } else {
+            if (position == length - 1) {
+                order = getString(R.string.firtime);
+            } else if (position == length - 2) {
+                order = getString(R.string.sectime);
+            } else {
+                order = String.valueOf(length - position) + ".";
+            }
+        }
+        return order;
     }
 }
