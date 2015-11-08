674eb8389bf7139d9a800bd66a56bda75fb74132
maxiee <maxieewong@gmail.com>
2015-11-08 21:18:35 +0800

    EventDetailActivity.java: fix event card background of the long image.

--- app/src/main/java/com/maxiee/heartbeat/ui/EventDetailActivity.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/EventDetailActivity.java
@@ -285,6 +285,7 @@ public class EventDetailActivity extends BaseActivity {
             progressDialog.show();
             FrameLayout mFl = new FrameLayout(EventDetailActivity.this);
             mFl.setBackgroundColor(ContextCompat.getColor(EventDetailActivity.this, R.color.window_background));
+            mCardEvent.setBackgroundColor(ContextCompat.getColor(EventDetailActivity.this, R.color.window_background));
             DisplayMetrics displaymetrics = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
             mWidth = displaymetrics.widthPixels;
