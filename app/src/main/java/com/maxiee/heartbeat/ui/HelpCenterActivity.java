f1f55829f3892ca15ee2bba24441668bb566b695
maxiee <maxieewong@gmail.com>
2015-12-29 15:54:44 +0800

    HelpCenterActivity.java: add 2 articles and adjust layout.

--- app/src/main/java/com/maxiee/heartbeat/ui/HelpCenterActivity.java
+++ app/src/main/java/com/maxiee/heartbeat/ui/HelpCenterActivity.java
@@ -20,15 +20,21 @@ import butterknife.ButterKnife;
 public class HelpCenterActivity extends BaseActivity {
 
     public static final String[] TITLES = new String[] {
-            /*1: */ "心动是一款怎样的记录软件"
+            /*1: */ "心动是一款怎样的记录软件",
+            /*2: */ "三级信息结构：标签－事件－感想",
+            /*999: */ "捐赠感谢名单"
     };
 
     public static final String[] DESCRIPTIONS = new String[] {
-            /*1: */ "心动是一款怎样的记录软件?背后的理念如何?本文将一一进行阐述……"
+            /*1: */ "心动是一款怎样的记录软件?背后的理念如何?本文将一一进行阐述……",
+            /*2: */ "正如前文中说的，心动改进了传统日记的信息组织方式，也就是心……",
+            /*999: */ "为了表达我的感谢，我建立了这个捐赠感谢名单，每一位捐赠用户……"
     };
 
     public static final String[] FILENAMES = new String[] {
-            /*1: */ "what_heartbeat_is.html"
+            /*1: */ "what_heartbeat_is.html",
+            /*2: */ "label_event_thought.html",
+            /*999: */ "donate_list.html"
     };
 
     @Bind(R.id.toolbar)         Toolbar         mToolbar;
