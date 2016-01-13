7f5e4bac5f40d71e3626d1f0c86794b710f909a8
maxiee <maxieewong@gmail.com>
2016-01-13 23:56:06 +0800

    Constant.java: URIs for ContentProvider.

---
+++ app/src/main/java/com/maxiee/heartbeat/provider/Constant.java
@@ -0,0 +1,13 @@
+package com.maxiee.heartbeat.provider;
+
+/**
+ * Created by maxiee on 16/1/10.
+ */
+public class Constant {
+    public static final String BASE_URI = "com.maxiee.heartbeat.provider";
+    public static final String CATEGORY_EVNET = "event/";
+
+    public static final String API_EVENT_RANDOM = CATEGORY_EVNET + "random";
+    public static final int API_EVENT_RANDOM_CODE = 0;
+
+}
