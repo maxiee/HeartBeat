dbbdfb3a1b6c4be36bf282f6a06da46447e51438
maxiee <maxieewong@gmail.com>
2016-04-15 17:13:24 +0800

    BackupManager: needTransGallery method.

--- app/src/main/java/com/maxiee/heartbeat/backup/BackupManager.java
+++ app/src/main/java/com/maxiee/heartbeat/backup/BackupManager.java
@@ -2,13 +2,24 @@ package com.maxiee.heartbeat.backup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 import com.maxiee.heartbeat.R;
 import com.maxiee.heartbeat.common.FileUtils;
+import com.maxiee.heartbeat.database.tables.ImageTable;
+import com.maxiee.heartbeat.database.tables.ThoughtResTable;
+import com.maxiee.heartbeat.database.utils.DatabaseUtils;
+import com.maxiee.heartbeat.database.utils.ImageUtils;
+import com.maxiee.heartbeat.database.utils.ThoughtUtils;
+import com.maxiee.heartbeat.model.Image;
+import com.maxiee.heartbeat.model.ThoughtRes;
+import com.maxiee.heartbeat.model.Thoughts;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -16,6 +27,7 @@ import java.io.OutputStream;
  * Created by maxiee on 15-8-10.
  */
 public class BackupManager {
+    public static final String TAG = BackupManager.class.getSimpleName();
     private final static String DB = "heartbeat";
     private final static String BACKUP_PATH = "HeartBeat";
     private final static String BACKUP_PREFIX = "[Backup]";
@@ -69,5 +81,73 @@ public class BackupManager {
             e.printStackTrace();
             return context.getString(R.string.restore_failed);
         }
+    }
+
+    public static boolean needTransGallery(Context context) {
+        boolean ret = false;
+        Cursor eventImage = DatabaseUtils.query(
+                context, ImageTable.NAME,
+                new String[] {ImageTable.ID, ImageTable.URI},
+                ImageTable.URI + " like ?", new String[] {"%/%"});
+        Log.d(TAG, String.format("事件图片有%d需要迁移.", eventImage.getCount()));
+        if (eventImage.getCount() > 1) ret = true;
+        eventImage.close();
+        if (ret) return true;
+
+        Cursor thoughtImage = DatabaseUtils.query(
+                context, ThoughtResTable.NAME,
+                new String[] {ThoughtResTable.ID, ThoughtResTable.TYPE,
+                        ThoughtResTable.PATH, ThoughtResTable.THOUGHT_ID},
+                ThoughtResTable.PATH + " like ?", new String[] {"%/%"});
+        Log.d(TAG, String.format("感想图片有%d需要迁移.", thoughtImage.getCount()));
+        if (thoughtImage.getCount() > 1) ret = true;
+        thoughtImage.close();
+        return ret;
+    }
+
+    public static void transGallery(Context context) {
+        Cursor eventImage = DatabaseUtils.query(
+                context, ImageTable.NAME,
+                new String[] {ImageTable.ID, ImageTable.URI},
+                ImageTable.URI + " like ?", new String[] {"%/%"});
+        Log.d(TAG, String.format("事件图片有%d需要迁移.", eventImage.getCount()));
+        int transCount = 0;
+        if (eventImage.getCount() > 1) {
+            while (eventImage.moveToNext()) {
+                Image i = ImageUtils.queryImage(eventImage);
+                try {
+                    String filename = FileUtils.copyImageToHeartBeat(i.getPath());
+                    ImageUtils.updateImageByImageId(context, i.getId(), filename);
+                    transCount++;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Log.d(TAG, String.format("成功迁移%d幅图片至心动目录.", transCount));
+        eventImage.close();
+        transCount = 0;
+
+        Cursor thoughtImage = DatabaseUtils.query(
+                context, ThoughtResTable.NAME,
+                new String[] {ThoughtResTable.ID, ThoughtResTable.TYPE,
+                        ThoughtResTable.PATH, ThoughtResTable.THOUGHT_ID},
+                ThoughtResTable.PATH + " like ?", new String[] {"%/%"});
+        Log.d(TAG, String.format("感想图片有%d需要迁移.", thoughtImage.getCount()));
+        if (thoughtImage.getCount() > 1) {
+            while (thoughtImage.moveToNext()) {
+                ThoughtRes r = ThoughtUtils.queryThoughtRes(thoughtImage);
+                if (r.getResType() != Thoughts.Thought.RES_IMAGE) continue;
+                try {
+                    String filename = FileUtils.copyImageToHeartBeat(r.getPath());
+                    ThoughtUtils.updateRes(context, r.getThoughtId(), r.getResType(), filename);
+                    transCount++;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Log.d(TAG, String.format("成功迁移%d幅图片至心动目录.", transCount));
+        thoughtImage.close();
     }
 }
